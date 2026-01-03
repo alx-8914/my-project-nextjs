14a164bb56aec1f8d304634024ba0d711611f5c6
alx-8914 <alexdevsilva@gmail.com>
2026-01-03 11:59:43 -0300

    atualização do arquivo pdf

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -31,7 +31,7 @@ export function HeroSection() {
       text: heroData.ctaPrimary.text,
       link: heroData.ctaPrimary.link,
       isDownload: true,
-      download: "CurricullumDev.pdf",
+      download: "Curricullum_Dev.pdf",
     },
   ]
 
